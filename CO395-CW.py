68167180b3adf5eeaa3a136114a9081b074638ec
LudwigAJ <ludwigalfred@gmail.com>
2020-10-29 17:32:24 +0000

    he

--- CO395-CW.py
+++ CO395-CW.py
@@ -1,70 +1,126 @@
+"""
+DESCRIPTION
+    A house has 7 WIFI hotspots pumping out Wi-Fi. Based on the signal strength of each one, we need to predict which room you are in.
+    We are given a 2000x8 array in .txt format. Where each column corresponds to the Wi-Fi strength originating from that hotspot.
+    i.e. The left-most column shows the strength a person is receiving from that specific hotspot (in dB I assume).
+    The right-most column states which room the person was in that instance.
+IMPLEMENTATION
+    Given that we have 7-attributes to choose from, we need to sort through each one that will give us the best IG.
+    We will have two custom types. One Node and one Tree. A Tree will simply contain Nodes starting from a root. 
+    The nodes will have a value to compare against which is the Wi-Fi hotspot we're currently using for comparison [0, 6].
+    They will also have an an attribute called attribute which says which Wi-Fi hotspot we're comparing. i.e. the index [0. 6].
+    Each node will also have a Room attribute. This is usually = None, but if it is not None, we have found a room. 
+    Each node will also have its two children defined as left and right. Of which they can be Node or None.
+    When going through the tree at a later stage, I guess we just compare the value from its attribute at a given node 
+    and if (it is less than or greater than?) I cannot think of which currently, but I think if it is less than the value at the node
+    we go left node and if not we go right. Or maybe it was vice versa lmao.
+"""
 import numpy as np
 
-def entropy(data):
-    _, counts = np.unique(data, return_counts=True)
-    pk = counts / sum(counts) 
-    return -sum(pk * np.log2(pk))
+def getEntropy(dataset):
+    datasetLabels = dataset[:, -1] # get only the labels to calculate pk.
+    _, occuranceArray = np.unique(datasetLabels, return_counts=True) # return the occurances of each sorted value(ascending).
+    pkArray = occuranceArray/np.sum(occuranceArray) # return the true divison of each value in an array.
+    entropy = -np.sum(pkArray * np.log2(pkArray)) # calculate the sum of each elementX element-wise multiplied with log2(elementX).
+    return entropy
 
-def information_gain(sall, sleft, sright):
-    remainder = (len(sleft)/len(sall)) * entropy(sleft) + (len(sright)/len(sall)) * entropy(sright)
-    return entropy(sall) - remainder
+def getRemainder(subsetLeft, subsetRight):
+    ssLeftSize, ssRightSize = len(subsetLeft), len(subsetRight) # get the amound of elements in the left and right subset
+    ssAllSize = ssLeftSize + ssRightSize # get the sum of elements in both subsets
+    leftValue = (ssLeftSize/ssAllSize) * getEntropy(subsetLeft) # : )
+    rightValue = (ssRightSize/ssAllSize) * getEntropy(subsetRight) # : )
+    remainder = leftValue + rightValue # calculate the remainder
+    return remainder
+
+def getInformationGain(dataset, subsetLeft, subsetRight):
+    informationGain = getEntropy(dataset) - getRemainder(subsetLeft, subsetRight)
+    return informationGain
+
+class Node:
+    def __init__(self, room=None, value=None, attribute=None, left=None, right=None):
+        self.room = room
+        self.value = value
+        self.attribute = attribute
+        self.left = left
+        self.right = right
 
 class Tree:
     def __init__(self, root=None):
         self.root = root
-
-    def decision_tree_learning(self, data, max_depth):
-        def decision_tree_learning_rec(self, data, depth):
-            if len(np.unique(data[:,-1])) == 1:
-                return Node(data[0][-1]), depth
-            else:
-                attribute, value = self.find_split(data)
-                return (attribute, value)
-
-    def find_split(self, data):
-        max_attr = 0
-        max_value = float("-inf")
-        max_ig = 0
-
-        labels = data[:,-1]
-
-        for attr_idx in range(len(data[0])-1):
-            sorted_idx = np.argsort(data[:,attr_idx]) # This gives the sorted array via the element's old index
-            sorted_labels = labels[sorted_idx] # This sorts the labels array withe the sorted_idx as the new indexs
-            sorted_col = data[:,attr_idx][sorted_idx]
+    
+    def decision_tree_learning(self, training_dataset, depth):
+        datasetLabels = training_dataset[:, -1] # split out so we get an 1-D array of all the labels
+        countLabels = np.unique(datasetLabels) # count the number of unique labels and their occurances
+        if len(countLabels) == 1: # if the length is == 1 we make a new tree node
             
-            cur_label = sorted_labels[0]
+            print("\033[1;32m")
+            print("LEAFNODEFOUND_START")
+            print("\033[0m")
+            print(training_dataset)
+            print("\033[1;32m")
+            print("LEAFNODEFOUND_END")
+            print("\033[0m")
 
-            for i in range(1,len(data)):
-                if sorted_labels[i] != cur_label:
-                    cur_label = sorted_labels[i]
-                    sleft = sorted_col[:i]
-                    sright = sorted_col[i:]
-                    ig = information_gain(sorted_col, sleft, sright)
+            return Node(room=countLabels[0]), depth # and return it here
+        else:
+            row, column = self.find_split(training_dataset) # remember, we need to sort the training set on the column it was retreived from.
+            training_dataset = training_dataset[training_dataset[:,column].argsort()] # sort the tree (ascending) for the column used to split.
+            tempNode = Node(value=training_dataset[row, column], attribute=column) # Make a new node and assign its right values.
+            if not(self.root):
+                self.root = tempNode
 
-                    if ig > max_ig:
-                        max_ig = ig
-                        max_attr = attr_idx
-                        max_value = sorted_col[i]
-        return max_attr, max_value
+            print("\033[1;33m")
+            print("Split into its LEFT part")
+            print("\033[0m")
+            print(training_dataset[:row, :])
+            print("\033[1;33m")
+            print("Split into its RIGHT part")
+            print("\033[0m")
+            print(training_dataset[row:, :])
 
+            tempNode.left, l_depth = self.decision_tree_learning(training_dataset[:row, :], depth+1) # Recursion!
+            tempNode.right, r_depth = self.decision_tree_learning(training_dataset[row:, :], depth+1)
+            return tempNode, max(l_depth, r_depth) #return the tempNode up the chain.
+    
+    def find_split(self, training_dataset):
+        
+        finalIGValue = float('-inf') # ref values for outer loop. -inf just as a lower boundary.
+        finalIGValueRow = float('-inf')
+        finalIGValueColumn = float('-inf')
 
-class Node:
-    def __init__(self, attribute, value):
-        self.attribute = attribute
-        self.value = value
-        self.left = None
-        self.right = None
+        print("\033[1;35m")
+        print("Training set currently finding split of")
+        print("\033[0m")
+        print(training_dataset)
 
+        for i in range(0, training_dataset.shape[1]-1):
+            training_dataset = training_dataset[training_dataset[:,i].argsort()] # sort the dataset by current column (specified by i)
+            maxIGValue = float('-inf') # ref values for inner loop
+            maxIGValueRow = float('-inf')
+            
+            for index in range(1, training_dataset.shape[0]):
+                
+                leftSubset = training_dataset[:index, :] # split the dataset into left and right
+                rightSubset = training_dataset[index:, :]
+                currentIG = getInformationGain(training_dataset, leftSubset, rightSubset) # see the IG it produces
+                if currentIG > maxIGValue: # If this is true, we have found a better split than before
+                    maxIGValue = currentIG
+                    maxIGValueRow = index
 
-class Leaf:
-    pass
+            if maxIGValue > finalIGValue: # If this is true, this column gave a better split than the others before
+                finalIGValue = maxIGValue
+                finalIGValueRow = maxIGValueRow
+                finalIGValueColumn = i
+        
+        return finalIGValueRow, finalIGValueColumn # return the best split on the form of: row, column
 
+def main():
+    text = np.loadtxt("wifi_db/clean_dataset.txt") # set everything up and run.
+    root = Node()
+    depth = 0
+    dTree = Tree()
+    root, depth = dTree.decision_tree_learning(text, depth)
 
-if __name__== "__main__":
-    path= 'wifi_db/clean_dataset.txt'
-    data = np.loadtxt(path)
-    root = Tree()
-    print(root.find_split(data))
-
-    # print(data)
+# At execution look for name __main__ and run it.
+if __name__ == "__main__":
+    main()
